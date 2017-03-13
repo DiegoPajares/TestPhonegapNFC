2539c3b7fc6bca38d0016e7a386a2112ac8bdbb9
Diegoupnorte <Diego@MacBook-Pro-de-Diego.local>
2017-03-13 00:02:42 -0500

    V 0.1

---
+++ platforms/blackberry10/platform_www/cordova_plugins.js
@@ -0,0 +1,28 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.chariotsolutions.nfc.plugin/www/phonegap-nfc.js",
+        "id": "com.chariotsolutions.nfc.plugin.NFC",
+        "pluginId": "com.chariotsolutions.nfc.plugin",
+        "runs": true
+    },
+    {
+        "file": "plugins/com.chariotsolutions.nfc.plugin/www/phonegap-nfc.js",
+        "id": "com.chariotsolutions.nfc.plugin.NFC",
+        "pluginId": "com.chariotsolutions.nfc.plugin",
+        "runs": true
+    },
+    {
+        "file": "plugins/com.chariotsolutions.nfc.plugin/www/phonegap-nfc-blackberry.js",
+        "id": "com.chariotsolutions.nfc.plugin.NFCBB10",
+        "pluginId": "com.chariotsolutions.nfc.plugin",
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.chariotsolutions.nfc.plugin": "0.4.6"
+}
+// BOTTOM OF METADATA
+});
